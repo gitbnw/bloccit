4181adceb7b574116d5e75bfb0ddac0c9eeb7961
gitbnw <byron.weiss@gmail.com>
2015-11-02 10:19:41 -0600

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,29 @@
  include RandomData
 
  # Create Posts
- 50.times do
+ # 50.times do
 
-   Post.create!(
+ #   Post.create!(
 
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
+ #     title:  RandomData.random_sentence,
+ #     body:   RandomData.random_paragraph
+ #   )
+ # end
  posts = Post.all
+
+Post.find_or_create_by(title: "Cool title", body: "Says stuff")
 
  # Create Comments
 
- 100.times do
-   Comment.create!(
+ # 100.times do
+ #   Comment.create!(
 
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+ #     post: posts.sample,
+ #     body: RandomData.random_paragraph
+ #   )
+ # end
+
+ Comment.find_or_create_by(body: "Never read the comments.")
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
