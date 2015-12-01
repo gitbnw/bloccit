a6b5ec435265f3e8a757a143d9a411f48ea9237d
gitbnw <byron.weiss@gmail.com>
2015-12-01 15:46:03 +0000

    Checkpoint 42 completed, labels

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,13 +14,13 @@ class PostsController < ApplicationController
   end
 
   def create
-
+    
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.build(post_params)
     @post.user = current_user
 
     if @post.save
-
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = 'Post was saved.'
       redirect_to [@topic, @post]
     else
@@ -39,6 +39,7 @@ class PostsController < ApplicationController
     @post.assign_attributes(post_params)
 
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was updated."
       redirect_to [@post.topic, @post]
     else
