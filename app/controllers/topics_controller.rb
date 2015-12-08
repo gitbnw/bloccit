90e19184d48b6a76de4ffb62efbbc172c58b7178
gitbnw <byron.weiss@gmail.com>
2015-12-08 15:13:42 +0000

    Checkpoint 46 private: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,10 +5,17 @@ class TopicsController < ApplicationController
    before_action :authorize_user, except: [:index, :show]
   
   def index
-    @topics = Topic.all
+    @topics = Topic.visible_to(current_user)
   end
   def show
     @topic = Topic.find(params[:id])
+
+     unless @topic.public || current_user
+       flash[:alert] = "You must be signed in to view private topics."
+       redirect_to root_url, notice: "Hello world"
+ 
+     end
+     
   end
   def new
     @topic = Topic.new
